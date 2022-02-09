211b7d581ca80ef83225cfb7a2734dfcec6366de
Ceciliamich <78112088+Ceciliamich@users.noreply.github.com>
2022-02-08 22:44:20 -0800

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,13 @@
+// We psedudocoded google's dino run game, we have no clue what you want us to write
+// Google Dino Run:
+  // Disconnect wifi or go to https://trex-runner.com/
+  // Go to google browser
+  // Press spacebar to begin game
+  // Use arrow keys to move up or down to avoid obstacles
+  // Don’t run into the obstacles
+    // Get a fucking high score.
+
+// @https://trex-runner.com/
+//@file this file defines the psedudocode
+// @author Cecilia Michel Puente google?
+//@since 8 22
