7be807df936184594ff1ea3aba3746ca79486cba
Ceciliamich <78112088+Ceciliamich@users.noreply.github.com>
2022-02-28 10:16:35 -0800

    lab 8 push

---
+++ lab8/js/lab8.js
@@ -0,0 +1,34 @@
+//Author: Cecilia Michel Puente
+// Created: February 24, 2022
+//License: Public Domain
+
+var ageNumber = [1,3,13,88,67,53,4.5]
+undefined
+function ageNumber(x){
+    return (x*2);
+}
+undefined
+ageNumber(2)
+4
+ageNumber(4)
+8
+console.log("Is my age odd?", ageNumber(1));
+VM1382:1 Is my age odd? 2
+undefined
+array
+(9) [89, 67, 73, 45, 23.3, 3.5, 7, 1, 0]
+array.map(ageNumber);
+(9) [178, 134, 146, 90, 46.6, 7, 14, 2, 0]
+array.map(function(x){
+    return x + 5;
+})
+(9) [94, 72, 78, 50, 28.3, 8.5, 12, 6, 5]
+
+
+function main() {
+    document.writeln("Here's your age: " + ageNumber + "</br></br>");
+    
+
+}
+
+main();
